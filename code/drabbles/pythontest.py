ab87a3fe404f19052c4fa2c131972b79bf54e8f3
GreggChase <94650453+GreggChase@users.noreply.github.com>
2024-10-16 13:39:20 -0400

    Update pythontest.py

--- code/drabbles/pythontest.py
+++ code/drabbles/pythontest.py
@@ -1,1 +1,22 @@
-
+print("hello World!")
+print("I'm Gregg")
+print(2+3)
+def addTwoNumbs(x,y):
+    return x+y
+print(addTwoNumbs(2,3))
+print(addTwoNumbs(addTwoNumbs(2,3),(3+4))+5)
+a=int(input("please enter your first number"))
+b=int(input("please enter your second number"))
+def doubleOrDiff(x,y):
+    if(x==y):
+     return addTwoNumbs(x,y)
+    elif(x>y):
+     return x-y
+    elif(y>x):
+     return y-x
+c=doubleOrDiff(a,b)
+print(c)
+print(doubleOrDiff(a,c))
+print(doubleOrDiff(b,c))
+print(a)
+print(b)
