b1fc5b5e03e179c1b4f7fefbc5e21f018b64502e
GreggChase <94650453+GreggChase@users.noreply.github.com>
2024-10-17 10:23:30 -0400

    Update pythontest.py
    fixed asin via recursion, broke case in 2 to aprox half time and space complexity

--- code/drabbles/pythontest.py
+++ code/drabbles/pythontest.py
@@ -1,3 +1,4 @@
+import math
 print("hello World!")
 print("I'm Gregg")
 print(2+3)
@@ -20,20 +21,29 @@ print(doubleOrDiff(a,c))
 print(doubleOrDiff(b,c))
 print(a)
 print(b)
-def Asin(x):
+def Asin(x, t):
   if abs(x)>1:
     return "error, no arcsine exists"
   if x==0:
     return 0
   else:
-    c= 2*pow((1-pow((1-x*x),1/2))/2,1/2)
-    if  x>0:
-      if sin (c) == x:
-        return c
-      else: 
-        return "error, math failed"
+    if math.sin(x)-x<t:
+      return x
+    elif x<(math.sqrt(2)/2):
+      a=x+math.sqrt(1-((t/4)*(t/4)))
+      return AsinDU(x,t,a)
     else:
-      if sin (c) == -x:
-        return -c
-      else:
-        return "error, math failed"
+      a=1
+      return AsinDD(x,t,a)
+def AsinDD(x,t,a):
+  if math.sin(a)-x<t:
+    return a
+  else:
+    a-=math.sqrt(1-((t/4)*(t/4)))
+    return AsinDD(x,t,a)
+def AsinDU(x,t,a):
+  if math.sin(a)-x<t:
+    return a
+  else:
+    a+=math.sqrt(1-((t/4)*(t/4)))
+    return AsinDU(x,t,a)
